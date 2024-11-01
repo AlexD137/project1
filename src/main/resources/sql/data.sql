cee77465c3cf7ce21747a2752fd92513a375e6b1
AlexD137 <blizkota@gmail.com>
2024-11-01 16:18:48 +0300

    Добавлена база данных, переписана логика для работы с БД

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (firstname, secondname, age)
+VALUES ('Ivan', 'Ivanov', 20),
+       ('Ivan2', 'Ivanov2', 22),
+       ('Ivan3', 'Ivanov3', 23),
+       ('Ivan4', 'Ivanov4', 25);
+
+
